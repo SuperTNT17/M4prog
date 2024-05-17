5810e2075bd1cffc0826c673bab851fcecb73baa
Robin Lemmers <38210@ma-web.nl>
2024-05-17 10:51:49 +0200

    02 data 01 data

---
+++ dockerM4Prog/sql/02 data/huisdierdata.sql
@@ -0,0 +1,11 @@
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`)
+VALUES
+(NULL, 'woef', 'robin', 'hond', '6', '2018-05-17'),
+(NULL, 'miauw', 'daniel', 'kat', '3', '2020-02-10');
+
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`)
+VALUES
+(NULL, 'daniel', 'daniel', 'kat', '13', '2011-10-20'),
+(NULL, 'abc', 'nibor', 'kaas', '1', '2024-02-11'),
+(NULL, 'waf', 'jan', 'hond', '7', '2017-12-01'),
+(NULL, 'medium', 'snackbar', 'patat', '1', '2024-02-10');
