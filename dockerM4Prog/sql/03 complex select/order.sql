a0701a7825f915e965b3262576dc8e383d400801
Robin Lemmers <38210@ma-web.nl>
2024-05-31 09:42:09 +0200

    03 order

---
+++ dockerM4Prog/sql/03 complex select/order.sql
@@ -0,0 +1,4 @@
+SELECT * FROM huisdieren order by naam ASC;
+SELECT * FROM huisdieren order by naam DESC;
+SELECT * FROM huisdieren order by typedier, naam; 
+SELECT * FROM huisdieren order by typedier ASC, naam DESC; 
