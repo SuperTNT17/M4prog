9c1758fedf2d29d173460b530d2200b64ada22e1
Robin Lemmers <38210@ma-web.nl>
2024-05-31 09:30:49 +0200

    les 2 en 3

---
+++ dockerM4Prog/sql/02 data/eigendata.sql
@@ -0,0 +1,5 @@
+INSERT INTO `auto` (`id`, `merk`, `model`, `prijs`, `kilometerstand`, `bouwjaar`) VALUES (NULL, 'lamborghini', 'abdul', '69000', '12000', '2024-02-09');
+INSERT INTO `auto` (`id`, `merk`, `model`, `prijs`, `kilometerstand`, `bouwjaar`) VALUES (NULL, 'kaas', 'oud', '2.00', '10', '2024-05-30');
+INSERT INTO `auto` (`id`, `merk`, `model`, `prijs`, `kilometerstand`, `bouwjaar`) VALUES (NULL, 'benefactor', 'schafter', '60000', '9000', '2014-09-12');
+INSERT INTO `auto` (`id`, `merk`, `model`, `prijs`, `kilometerstand`, `bouwjaar`) VALUES (NULL, 'dewbauchee', 'rapid gt', '135000', '16000', '2015-05-20');
+INSERT INTO `auto` (`id`, `merk`, `model`, `prijs`, `kilometerstand`, `bouwjaar`) VALUES (NULL, 'vysser', 'neo', '1875000', '5000', '2019-08-01');
---
+++ dockerM4Prog/sql/03 complex select/rekenen.sql
@@ -0,0 +1,4 @@
+SELECT sum(leeftijd) FROM huisdieren;
+SELECT AVG(leeftijd) FROM huisdieren; 
+SELECT SUM(leeftijd), AVG(leeftijd) FROM huisdieren; 
+SELECT sum(leeftijd) as gemiddeldeLeeftijd FROM huisdieren;
