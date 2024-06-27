873816f80e4704a8f555807768c06ba1c141fb5e
Robin Lemmers <38210@ma-web.nl>
2024-06-27 09:57:58 +0200

    05 normaliseren 01 eigenaar

---
+++ dockerM4Prog/sql/05 normaliseren/eigenaar.sql
@@ -0,0 +1,6 @@
+INSERT INTO `huisdiereigenaar` (`id`, `voornaam`, `achternaam`, `leeftijd`) VALUES (NULL, 'Robin', 'Lemmers', '20');
+INSERT INTO `huisdiereigenaar` (`id`, `voornaam`, `achternaam`, `leeftijd`) VALUES (NULL, 'Daniel', 'Wang', '19');
+INSERT INTO `huisdiereigenaar` (`id`, `voornaam`, `achternaam`, `leeftijd`) VALUES (NULL, 'Nibor', 'Sremmel', '20');
+INSERT INTO `huisdiereigenaar` (`id`, `voornaam`, `achternaam`, `leeftijd`) VALUES (NULL, 'Jan', 'Patat', '30');
+INSERT INTO `huisdiereigenaar` (`id`, `voornaam`, `achternaam`, `leeftijd`) VALUES (NULL, 'Snackbar', 'Gebouw', '15');
+INSERT INTO `huisdiereigenaar` (`id`, `voornaam`, `achternaam`, `leeftijd`) VALUES (NULL, 'Joep', 'Noob', '13');
